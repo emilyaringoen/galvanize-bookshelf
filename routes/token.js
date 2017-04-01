347de85dcd5f227d8b1f885ec1b80930f6c594f6
Emily Ringoen <emilyaringoen@gmail.com>
2017-03-31 23:09:32 -0600

    Fixed the final bug, it works

--- routes/token.js
+++ routes/token.js
@@ -1,24 +1,48 @@
-'use strict'
+'use strict';
 
 const express = require('express')
 const router = express.Router()
+const boom = require('boom')
 const humps = require('humps')
+const jwt = require('jsonwebtoken')
 const knex = require('../knex')
 const bcrypt = require('bcrypt')
-const boom = require('boom')
-const cookie = require('cookie-session')
-const jwt = require('jsonwebtoken');
-
-let authenticationStatus = false;
 
 router.get('/token', (req, res, next) => {
-  res.send(authenticationStatus)
+  if (!req.cookies.token) {
+    res.status(200).send(false)
+  } else {
+    res.status(200).send(true)
+  }
 })
 
 router.post('/token', (req, res, next) => {
-  
+  let email = req.body.email
+  let password = req.body.password
+  knex('users')
+    .where('email', email)
+    .then((data) => {
+      if (data.length > 0) {
+        bcrypt.compare(password, data[0].hashed_password, (err, boolean) => {
+          if (boolean) {
+            let token = jwt.sign({ email: data[0].email, password: data[0].hashed_password}, "shhh");
+            res.cookie('token', token, { httpOnly:true })
+            delete data[0].hashed_password
+            res.send(humps.camelizeKeys(data[0]))
+          } else {
+            next(boom.create(400, 'Bad email or password'))
+          }
+        })
+      } else {
+        next(boom.create(400, 'Bad email or password'))
+      }
+    })
 })
 
+router.delete('/token', (req, res, session) => {
+  res.clearCookie('token')
+  res.send(true)
+})
 
 
 module.exports = router;
